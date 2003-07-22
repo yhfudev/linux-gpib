cf8e6dc0932ef223a57483d18b91fc8f62ffdf43
nobody <nobody@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2003-07-22 21:49:12 +0000

    This commit was manufactured by cvs2svn to create tag 'v3_1_95'.

---
+++ tags/v3_1_95/linux-gpib/driver/tms9914/read.c
@@ -0,0 +1,141 @@
+/***************************************************************************
+                              tms9914/read.c
+                             -------------------
+
+    begin                : Dec 2001
+    copyright            : (C) 2001, 2002 by Frank Mori Hess
+    email                : fmhess@users.sourceforge.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "board.h"
+#include <linux/spinlock.h>
+
+void check_for_eos( tms9914_private_t *priv, uint8_t byte )
+{
+	if( ( priv->eos_flags & REOS ) == 0 ) return;
+
+	if( priv->eos_flags & BIN )
+	{
+		if( priv->eos == byte )
+			set_bit( RECEIVED_END_BN, &priv->state );
+	}else
+	{
+		if( ( priv->eos & 0x7f ) == ( byte & 0x7f ) )
+			set_bit( RECEIVED_END_BN, &priv->state );
+	}
+
+	if( test_bit( RECEIVED_END_BN, &priv->state ) == 0 )
+		write_byte( priv, AUX_RHDF, AUXCR );
+}
+
+static ssize_t pio_read(gpib_board_t *board, tms9914_private_t *priv, uint8_t *buffer, size_t length)
+{
+	size_t count = 0;
+	ssize_t retval = 0;
+	unsigned long flags;
+
+	while(count < length)
+	{
+		if(wait_event_interruptible(board->wait,
+			test_bit( READ_READY_BN, &priv->state ) ||
+			test_bit( DEV_CLEAR_BN, &priv->state ) ||
+			test_bit( TIMO_NUM, &board->status ) ) )
+		{
+			printk("gpib: pio read wait interrupted\n");
+			retval = -ERESTARTSYS;
+			break;
+		};
+		if( test_bit( TIMO_NUM, &board->status ) )
+		{
+			retval = -ETIMEDOUT;
+			break;
+		}
+		if( test_bit( DEV_CLEAR_BN, &priv->state ) )
+		{
+			retval = -EINTR;
+			break;
+		}
+
+		spin_lock_irqsave( &board->spinlock, flags );
+		clear_bit( READ_READY_BN, &priv->state );
+		buffer[ count++ ] = read_byte( priv, DIR );
+		spin_unlock_irqrestore( &board->spinlock, flags );
+
+		check_for_eos( priv, buffer[ count - 1 ] );
+
+		if( test_bit( RECEIVED_END_BN, &priv->state ) )
+			break;
+	}
+
+	return retval ? retval : count;
+}
+
+ssize_t tms9914_read(gpib_board_t *board, tms9914_private_t *priv, uint8_t *buffer, size_t length, int *end)
+{
+	size_t	count = 0;
+	ssize_t retval = 0;
+
+	*end = 0;
+
+	if(length == 0) return 0;
+
+	clear_bit( DEV_CLEAR_BN, &priv->state );
+
+	if( priv->eos_flags & REOS )
+	{
+		write_byte(priv, AUX_HLDA | AUX_CS, AUXCR);
+		write_byte(priv, AUX_HLDE, AUXCR);
+	}else
+	{
+		write_byte(priv, AUX_HLDA, AUXCR);
+		write_byte(priv, AUX_HLDE | AUX_CS, AUXCR);
+	}
+	write_byte(priv, AUX_RHDF, AUXCR);
+
+	// transfer data (except for last byte)
+	length--;
+	if(length)
+	{
+		// PIO transfer
+		retval = pio_read(board, priv, buffer, length);
+		if(retval < 0)
+			return retval;
+		else
+			count += retval;
+	}
+
+	// read last byte if we havn't received an END yet
+	if(test_bit(RECEIVED_END_BN, &priv->state) == 0)
+	{
+		// make sure we holdoff after last byte read
+		write_byte(priv, AUX_HLDE, AUXCR);
+		write_byte(priv, AUX_HLDA | AUX_CS, AUXCR);
+		retval = pio_read(board, priv, &buffer[count], 1);
+		if(retval < 0)
+			return retval;
+		else
+			count++;
+	}
+
+	if(test_and_clear_bit(RECEIVED_END_BN, &priv->state))
+		*end = 1;
+
+	return count;
+}
+
+EXPORT_SYMBOL(tms9914_read);
+
+
+
+
+
+
---
+++ tags/v3_1_95/linux-gpib/driver/tnt4882/tnt4882_read.c
@@ -0,0 +1,196 @@
+/***************************************************************************
+                          tnt4882_read.c  -  description
+                             -------------------
+
+    copyright            : (C) 2003 by Frank Mori Hess
+    email                : fmhess@users.sourceforge.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "tnt4882.h"
+#include <linux/delay.h>
+
+static int fifo_word_available( tnt4882_private_t *tnt_priv )
+{
+	int status2;
+	int retval;
+
+	status2 = tnt_readb( tnt_priv, STS2 );
+	retval = ( status2 & AEFN ) && ( status2 & BEFN );
+
+	return retval;
+}
+
+static int fifo_byte_available( tnt4882_private_t *tnt_priv )
+{
+	int status2;
+	int retval;
+
+	status2 = tnt_readb( tnt_priv, STS2 );
+	retval = ( status2 & AEFN ) || ( status2 & BEFN );
+
+	return retval;
+}
+
+static int fifo_xfer_done( tnt4882_private_t *tnt_priv )
+{
+	int status1;
+	int retval;
+
+	status1 = tnt_readb( tnt_priv, STS1 );
+	retval = status1 & ( S_DONE | S_HALT );
+
+	return retval;
+}
+
+ssize_t tnt4882_accel_read( gpib_board_t *board, uint8_t *buffer, size_t length, int *end )
+{
+	size_t count = 0;
+	ssize_t retval = 0;
+	tnt4882_private_t *tnt_priv = board->private_data;
+	nec7210_private_t *nec_priv = &tnt_priv->nec7210_priv;
+	unsigned long iobase = nec_priv->iobase;
+	unsigned int bits, imr1_bits, imr2_bits;
+	int32_t hw_count;
+	unsigned long flags;
+
+	nec7210_set_handshake_mode( board, nec_priv, HR_HLDA );
+	write_byte( nec_priv, AUX_FH, AUXMR );
+	nec7210_set_handshake_mode( board, nec_priv, HR_HLDE );
+
+	imr1_bits = nec_priv->reg_bits[ IMR1 ];
+	imr2_bits = nec_priv->reg_bits[ IMR2 ];
+	nec7210_set_reg_bits( nec_priv, IMR1, 0xff, HR_ENDIE | HR_DECIE );
+	nec7210_set_reg_bits( nec_priv, IMR2, 0xff, HR_DMAI );
+
+	tnt_writeb( tnt_priv, RESET_FIFO, CMDR );
+	udelay(1);
+
+	tnt_writeb( tnt_priv, nec_priv->auxa_bits | HR_HLDA, CCR );
+	bits = TNT_TLCHE | TNT_B_16BIT | TNT_IN | TNT_CCEN;
+	tnt_writeb( tnt_priv, bits, CFG );
+
+	// load 2's complement of count into hardware counters
+	hw_count = -length;
+	tnt_writeb( tnt_priv, hw_count & 0xff, CNT0 );
+	tnt_writeb( tnt_priv, ( hw_count >> 8 ) & 0xff, CNT1 );
+	tnt_writeb( tnt_priv, ( hw_count >> 16 ) & 0xff, CNT2 );
+	tnt_writeb( tnt_priv, ( hw_count >> 24 ) & 0xff, CNT3 );
+
+	tnt_writeb( tnt_priv, GO, CMDR );
+	udelay(1);
+
+	spin_lock_irqsave( &board->spinlock, flags );
+	tnt_priv->imr3_bits |= HR_DONE | HR_NEF;
+	tnt_writeb( tnt_priv, tnt_priv->imr3_bits, IMR3 );
+	spin_unlock_irqrestore( &board->spinlock, flags );
+
+	while( count + 1 < length &&
+		test_bit( RECEIVED_END_BN, &nec_priv->state ) == 0 )
+	{
+		// wait until byte is ready
+		if( wait_event_interruptible( board->wait,
+			fifo_word_available( tnt_priv ) ||
+			fifo_xfer_done( tnt_priv ) ||
+			test_bit( RECEIVED_END_BN, &nec_priv->state ) ||
+			test_bit( DEV_CLEAR_BN, &nec_priv->state ) ||
+			test_bit( TIMO_NUM, &board->status ) ) )
+		{
+			printk("gpib write interrupted\n");
+			retval = -ERESTARTSYS;
+			break;
+		}
+		if( test_bit( TIMO_NUM, &board->status ) )
+		{
+			retval = -ETIMEDOUT;
+			break;
+		}
+		if( test_bit( DEV_CLEAR_BN, &nec_priv->state ) )
+		{
+			retval = -EINTR;
+			break;
+		}
+
+		spin_lock_irqsave( &board->spinlock, flags );
+		while( fifo_word_available( tnt_priv ) && count + 1 < length )
+		{
+			uint16_t word;
+
+			word = tnt_priv->io_readw( iobase + FIFOB );
+			buffer[ count++ ] = word & 0xff;
+			buffer[ count++ ] = ( word >> 8 ) & 0xff;
+		}
+		tnt_priv->imr3_bits |= HR_NEF;
+		tnt_writeb( tnt_priv, tnt_priv->imr3_bits, IMR3 );
+		spin_unlock_irqrestore( &board->spinlock, flags );
+
+		if( current->need_resched )
+			schedule();
+	}
+	// wait for last byte
+	if( count < length )
+	{
+		if( wait_event_interruptible( board->wait,
+			fifo_byte_available( tnt_priv ) ||
+			fifo_xfer_done( tnt_priv ) ||
+			test_bit( RECEIVED_END_BN, &nec_priv->state ) ||
+			test_bit( DEV_CLEAR_BN, &nec_priv->state ) ||
+			test_bit( TIMO_NUM, &board->status ) ) )
+		{
+			printk("gpib write interrupted\n");
+			retval = -ERESTARTSYS;
+		}
+		if( test_bit( TIMO_NUM, &board->status ) )
+		{
+			retval = -ETIMEDOUT;
+		}
+		if( test_bit( DEV_CLEAR_BN, &nec_priv->state ) )
+		{
+			retval = -EINTR;
+		}
+		if( fifo_byte_available( tnt_priv ) )
+		{
+			buffer[ count++ ] = tnt_readb( tnt_priv, FIFOB );
+		}
+	}
+	if( test_and_clear_bit( RECEIVED_END_BN, &nec_priv->state ) )
+	{
+		*end = 1;
+	}
+
+	tnt_writeb( tnt_priv, STOP, CMDR );
+	udelay(1);
+
+	nec7210_set_reg_bits( nec_priv, IMR1, 0xff, imr1_bits );
+	nec7210_set_reg_bits( nec_priv, IMR2, 0xff, imr2_bits );
+
+	if( retval < 0 )
+	{
+		// force immediate holdoff
+		write_byte( nec_priv, AUX_HLDI, AUXMR );
+		set_bit( RFD_HOLDOFF_BN, &nec_priv->state );
+		return retval;
+	}
+
+	return count;
+}
+
+
+
+
+
+
+
+
+
+
+
+
