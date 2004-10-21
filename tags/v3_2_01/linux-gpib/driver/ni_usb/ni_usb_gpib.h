437fc674e51961bc0503ff911cd361c3ec11a646
nobody <nobody@b61deed1-901b-0410-a6dd-e94ecbdaea89>
2004-10-21 23:09:41 +0000

    This commit was manufactured by cvs2svn to create tag 'v3_2_01'.

---
+++ tags/v3_2_01/linux-gpib/driver/ni_usb/ni_usb_gpib.h
@@ -0,0 +1,150 @@
+/***************************************************************************
+                              ni_usb_gpib.h
+                             -------------------
+
+    begin                : May 2004
+    copyright            : (C) 2004 by Frank Mori Hess
+    email                : fmhess@users.sourceforge.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _NI_USB_GPIB_H
+#define _NI_USB_GPIB_H
+
+#include <linux/usb.h>
+#include "gpibP.h"
+
+enum 
+{
+	USB_VENDOR_ID_NI = 0x3923
+};
+
+enum
+{
+	USB_DEVICE_ID_NI_USB_B = 0x702a,
+	USB_DEVICE_ID_NI_USB_B_PREINIT = 0x702b	// device id before firmware is loaded
+};
+
+enum ni_usb_device
+{
+	NIUSB_SUBDEV_TNT4882 = 1,
+	NIUSB_SUBDEV_UNKNOWN2 = 2,
+	NIUSB_SUBDEV_UNKNOWN3 = 3,
+};
+
+enum endpoint_addresses
+{
+	NIUSB_BULK_OUT_ENDPOINT = 0x2,
+	NIUSB_BULK_IN_ENDPOINT = 0x82,
+	NIUSB_INTERRUPT_IN_ENDPOINT = 0x84,
+};
+
+// struct which defines private_data for ni_usb devices
+typedef struct
+{
+	struct usb_interface *bus_interface;
+	uint8_t eos_char;
+	unsigned short eos_mode;
+	unsigned int monitored_ibsta_bits;
+	struct urb *interrupt_urb;
+	uint8_t interrupt_buffer[0x11];
+	struct semaphore bulk_transfer_lock;
+	struct semaphore control_transfer_lock;
+	struct semaphore interrupt_transfer_lock;
+} ni_usb_private_t;
+
+struct ni_usb_status_block
+{
+	short id;
+	unsigned short ibsta;
+	short error_code;
+	unsigned short count;
+};
+
+struct ni_usb_register
+{
+	enum ni_usb_device device;
+	short address;
+	unsigned short value;
+};
+
+enum ni_usb_bulk_ids
+{
+	NIUSB_IBCAC_ID = 0x1,
+	NIUSB_UNKNOWN3_ID = 0x3, // device level function id?
+	NIUSB_TERM_ID = 0x4,
+	NIUSB_IBGTS_ID = 0x6,
+	NIUSB_IBRPP_ID = 0x7,
+	NIUSB_REG_READ_ID = 0x8,
+	NIUSB_REG_WRITE_ID = 0x9,
+	NIUSB_IBSIC_ID = 0xf,
+	NIUSB_REGISTER_READ_DATA_START_ID = 0x34,
+	NIUSB_REGISTER_READ_DATA_END_ID = 0x35,
+	NIUSB_IBRD_DATA_ID = 0x36,
+	NIUSB_IBRD_STATUS_ID = 0x38
+};
+
+static const unsigned int ni_usb_ibsta_monitor_mask = SRQI | LOK | REM | CIC | ATN | TACS | LACS | DTAS | DCAS;
+
+static inline int nec7210_to_tnt4882_offset(int offset)
+{
+	return 2 * offset;
+};
+static inline int ni_usb_bulk_termination(uint8_t *buffer)
+{
+	int i = 0;
+	
+	buffer[i++] = NIUSB_TERM_ID;
+	buffer[i++] = 0x0;
+	buffer[i++] = 0x0;
+	buffer[i++] = 0x0;
+	return i;	
+}
+
+static inline int ni_usb_bulk_register_write_header(uint8_t *buffer, int num_writes)
+{
+	int i = 0;
+	
+	buffer[i++] = NIUSB_REG_WRITE_ID;
+	buffer[i++] = num_writes;
+	buffer[i++] = 0x0;
+	return i;	
+}
+
+static inline int ni_usb_bulk_register_write(uint8_t *buffer, struct ni_usb_register reg)
+{
+	int i = 0;
+	
+	buffer[i++] = reg.device;
+	buffer[i++] = reg.address;
+	buffer[i++] = reg.value;
+	return i;	
+}
+
+static inline int ni_usb_bulk_register_read_header(uint8_t *buffer, int num_reads)
+{
+	int i = 0;
+	
+	buffer[i++] = NIUSB_REG_READ_ID;
+	buffer[i++] = num_reads;
+	return i;	
+}
+
+static inline int ni_usb_bulk_register_read(uint8_t *buffer, int device, int address)
+{
+	int i = 0;
+	
+	buffer[i++] = device;
+	buffer[i++] = address;
+	return i;	
+}
+
+#endif	// _NI_USB_GPIB_H
---
+++ tags/v3_2_01/linux-gpib/driver/tms9914/read.c
@@ -0,0 +1,139 @@
+/***************************************************************************
+                              tms9914/read.c
+                             -------------------
+
+    begin                : Dec 2001
+    copyright            : (C) 2001, 2002 by Frank Mori Hess
+    email                : fmhess@users.sourceforge.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "board.h"
+#include <linux/spinlock.h>
+
+void check_for_eos( tms9914_private_t *priv, uint8_t byte )
+{
+	if( ( priv->eos_flags & REOS ) == 0 ) return;
+
+	if( priv->eos_flags & BIN )
+	{
+		if( priv->eos == byte )
+			set_bit( RECEIVED_END_BN, &priv->state );
+	}else
+	{
+		if( ( priv->eos & 0x7f ) == ( byte & 0x7f ) )
+			set_bit( RECEIVED_END_BN, &priv->state );
+	}
+
+	if( test_bit( RECEIVED_END_BN, &priv->state ) == 0 )
+		write_byte( priv, AUX_RHDF, AUXCR );
+}
+
+static ssize_t pio_read(gpib_board_t *board, tms9914_private_t *priv, uint8_t *buffer, size_t length, int *nbytes)
+{
+	ssize_t retval = 0;
+	unsigned long flags;
+
+	*nbytes = 0;
+	while(*nbytes < length)
+	{
+		if(wait_event_interruptible(board->wait,
+			test_bit( READ_READY_BN, &priv->state ) ||
+			test_bit( DEV_CLEAR_BN, &priv->state ) ||
+			test_bit( TIMO_NUM, &board->status ) ) )
+		{
+			printk("gpib: pio read wait interrupted\n");
+			retval = -ERESTARTSYS;
+			break;
+		};
+		if( test_bit( TIMO_NUM, &board->status ) )
+		{
+			retval = -ETIMEDOUT;
+			break;
+		}
+		if( test_bit( DEV_CLEAR_BN, &priv->state ) )
+		{
+			retval = -EINTR;
+			break;
+		}
+
+		spin_lock_irqsave( &board->spinlock, flags );
+		clear_bit( READ_READY_BN, &priv->state );
+		buffer[ (*nbytes)++ ] = read_byte( priv, DIR );
+		spin_unlock_irqrestore( &board->spinlock, flags );
+
+		check_for_eos( priv, buffer[ *nbytes - 1 ] );
+
+		if( test_bit( RECEIVED_END_BN, &priv->state ) )
+			break;
+	}
+
+	return retval;
+}
+
+ssize_t tms9914_read(gpib_board_t *board, tms9914_private_t *priv, uint8_t *buffer, size_t length, int *end, int *nbytes)
+{
+	ssize_t retval = 0;
+	int bytes_read;
+	
+	*end = 0;
+	*nbytes = 0;
+	if(length == 0) return 0;
+
+	clear_bit( DEV_CLEAR_BN, &priv->state );
+
+	if( priv->eos_flags & REOS )
+	{
+		write_byte(priv, AUX_HLDA | AUX_CS, AUXCR);
+		write_byte(priv, AUX_HLDE, AUXCR);
+	}else
+	{
+		write_byte(priv, AUX_HLDA, AUXCR);
+		write_byte(priv, AUX_HLDE | AUX_CS, AUXCR);
+	}
+	write_byte(priv, AUX_RHDF, AUXCR);
+
+	// transfer data (except for last byte)
+	length--;
+	if(length)
+	{
+		// PIO transfer
+		retval = pio_read(board, priv, buffer, length, &bytes_read);
+		*nbytes += bytes_read;
+		if(retval < 0)
+			return retval;
+	}
+
+	// read last byte if we havn't received an END yet
+	if(test_bit(RECEIVED_END_BN, &priv->state) == 0)
+	{
+		// make sure we holdoff after last byte read
+		write_byte(priv, AUX_HLDE, AUXCR);
+		write_byte(priv, AUX_HLDA | AUX_CS, AUXCR);
+		retval = pio_read(board, priv, &buffer[*nbytes], 1, &bytes_read);
+		*nbytes += bytes_read;
+		if(retval < 0)
+			return retval;
+	}
+
+	if(test_and_clear_bit(RECEIVED_END_BN, &priv->state))
+		*end = 1;
+
+	return 0;
+}
+
+EXPORT_SYMBOL(tms9914_read);
+
+
+
+
+
+
